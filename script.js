3ff4122182d619b0f4966d1fca650da93ffdbcdc
aaronh1986 <aaronharbourne@gmail.com>
2021-06-05 10:14:17 +0000

    4:23

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var apiUrl = 60847a1ec9175abe97b3d9d6e8784457 api.openweathermap.org/data/2.5/weather?q=dublin&appid=60847a1ec9175abe97b3d9d6e8784457
+var apiUrl = "api.openweathermap.org/data/2.5/weather?q=dublin&units=metric&appid=f070283aa2ae3b489a7140182993e9d8"
+
+var weatherInfo = document.getElementById('weatherInfo');
+
+function getWeatherInfo() {
+    var place = document.getElementById('place').value.trim();
+}
